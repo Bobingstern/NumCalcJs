81cf89bf5ca813aafdedb6ff4ff09bcf4bbd66b8
Replit user <>
2023-01-23 23:23:27 +0000

    Add NDerivative and improve documentation

--- NumCalcJs/NumCalc.js
+++ NumCalcJs/NumCalc.js
@@ -1,138 +1,131 @@
-const NumCalc_GR = (Math.sqrt(5)+1)/2
-const NumCalc_EPSILON = 2**(-23);
+const NumCalc_GR = (Math.sqrt(5) + 1) / 2
+const NumCalc_EPSILON = 2 ** (-23);
 const NumCalc_ROOT_EPSILON = Math.sqrt(NumCalc_EPSILON)
 const NumCalc = {
 
-  maximum: (f,a,b,error) => {
+  maximum: (f, a, b, error) => {
     error = error == undefined ? 1e-4 : error
-    let c = b - (b-a) / NumCalc_GR
-    let d = a + (b-a) / NumCalc_GR
-    while (Math.abs(b-a) > error){
-      if (f(c) > f(d)){
+    let c = b - (b - a) / NumCalc_GR
+    let d = a + (b - a) / NumCalc_GR
+    while (Math.abs(b - a) > error) {
+      if (f(c) > f(d)) {
         b = d
       }
-      else{
-        a=c
+      else {
+        a = c
       }
-      c = b - (b-a) / NumCalc_GR
-      d = a + (b-a) / NumCalc_GR
+      c = b - (b - a) / NumCalc_GR
+      d = a + (b - a) / NumCalc_GR
     }
-    return (a+b)/2
+    return (a + b) / 2
   },
-  
-  minimum: (f,a,b,error) => {
+
+  minimum: (f, a, b, error) => {
     error = error == undefined ? 1e-4 : error
-    let c = b - (b-a) / NumCalc_GR
-    let d = a + (b-a) / NumCalc_GR
-    while (Math.abs(b-a) > error){
-      if (f(c) < f(d)){
+    let c = b - (b - a) / NumCalc_GR
+    let d = a + (b - a) / NumCalc_GR
+    while (Math.abs(b - a) > error) {
+      if (f(c) < f(d)) {
         b = d
       }
-      else{
-        a=c
+      else {
+        a = c
       }
-      c = b - (b-a) / NumCalc_GR
-      d = a + (b-a) / NumCalc_GR
+      c = b - (b - a) / NumCalc_GR
+      d = a + (b - a) / NumCalc_GR
     }
-    return (a+b)/2
+    return (a + b) / 2
   },
-  
-  nCr: (n,k) => {
+
+  nCr: (n, k) => {
     var result = 1;
-    for(var i=1; i <= k; i++){
-        result *= (n+1-i)/i;
+    for (var i = 1; i <= k; i++) {
+      result *= (n + 1 - i) / i;
     }
     return result;
   },
-  
-  // function NDerivative(f,x,n){
-  //   n = n == undefined ? 1 : n
-  //   let h = ROOT_EPSILON*(Math.abs(x) + ROOT_EPSILON)*n
-  //   let sum = 0
-  //   for (let k=0;k<=n;k++){
-  //     sum += (-1)**k * nCr(n,k) * f(x + (n-2*k)*h)
-  //   }
-  //   return 1/((2*h)**n)*sum
-  // }
-  
-  FDerivative: (f,n) => {
+
+  NDerivative: (f, x, n) => {
     n = n == undefined ? 1 : n
-    function d(x){
-      let h = NumCalc_ROOT_EPSILON*(Math.abs(x) + NumCalc_ROOT_EPSILON)*n**2
-      let sum = 0
-      for (let k=0;k<=n;k++){
-        sum += (-1)**k * NumCalc.nCr(n,k) * f(x + (n-2*k)*h)
-      }
-      return 1/((2*h)**n)*sum
+    let h = NumCalc_ROOT_EPSILON * (Math.abs(x) + NumCalc_ROOT_EPSILON) * n ** 2
+    let sum = 0
+    for (let k = 0; k <= n; k++) {
+      sum += (-1) ** k * NumCalc.nCr(n, k) * f(x + (n - 2 * k) * h)
     }
-    return d
+    return 1 / ((2 * h) ** n) * sum
   },
-  NIntegrate: (f,a,b,error) => {
+
+  FDerivative: (f, n) => {
+    n = n == undefined ? 1 : n
+    return (x) => { return NumCalc.NDerivative(f, x, n); };
+  },
+
+  NIntegrate: (f, a, b, error) => {
     error = error == undefined ? 1e-4 : error
     let lower = a
     let upper = b
     let integrand;
-  
-    if (b == Number.NEGATIVE_INFINITY || a == Number.POSITIVE_INFINITY){
+
+    if (b == Number.NEGATIVE_INFINITY || a == Number.POSITIVE_INFINITY) {
       console.log("Improper Integral Error, make sure your bounds are in the correct order when using improper integrals")
-      return 
+      return
     }
-    
-    if (b < a){
-      integrand = function(x){
+
+    if (b < a) {
+      integrand = function(x) {
         return -f(x)
       }
       lower = b
       upper = a
     }
-    else if (b == Number.POSITIVE_INFINITY && isFinite(a)){
-      integrand = function(x){
-        if (x == 1){
-          x-=0.000001 //1/0 error prevention
+    else if (b == Number.POSITIVE_INFINITY && isFinite(a)) {
+      integrand = function(x) {
+        if (x == 1) {
+          x -= 0.000001 //1/0 error prevention
         }
-        return f(a + x/(1-x))/((1-x)**2)
+        return f(a + x / (1 - x)) / ((1 - x) ** 2)
       }
       lower = 0
       upper = 1
     }
-    else if (a == Number.NEGATIVE_INFINITY && isFinite(b)){
-      integrand = function(x){
-        if (x == 0){
-          x+=0.000001 //1/0 error prevention
+    else if (a == Number.NEGATIVE_INFINITY && isFinite(b)) {
+      integrand = function(x) {
+        if (x == 0) {
+          x += 0.000001 //1/0 error prevention
         }
-        return f(b-(1-x)/x)/x**2
+        return f(b - (1 - x) / x) / x ** 2
       }
       lower = 0
       upper = 1
     }
-    else if (a == Number.NEGATIVE_INFINITY && b == Number.POSITIVE_INFINITY){
-      integrand = function(x){
-        if (x == 1){
-          x-=0.000001 //1/0 error prevention
+    else if (a == Number.NEGATIVE_INFINITY && b == Number.POSITIVE_INFINITY) {
+      integrand = function(x) {
+        if (x == 1) {
+          x -= 0.000001 //1/0 error prevention
         }
-        if (x == -1){
-          x+=0.000001 //1/0 error prevention
+        if (x == -1) {
+          x += 0.000001 //1/0 error prevention
         }
-        return f(x/(1-x**2)) * ((1+x**2)/((1-x**2)**2))
+        return f(x / (1 - x ** 2)) * ((1 + x ** 2) / ((1 - x ** 2) ** 2))
       }
       lower = -1
       upper = 1
     }
-    else{
-      integrand = function(x){
+    else {
+      integrand = function(x) {
         return f(x)
       }
     }
-    
-    
-    let M = NumCalc.maximum(NumCalc.FDerivative(integrand),lower,upper)
-    let N = Math.ceil(Math.pow((Math.abs(M)*(upper-lower)**5)/(180*error),1/4))+30
-    let q = (upper-lower)/N
+
+
+    let M = NumCalc.maximum(NumCalc.FDerivative(integrand), lower, upper)
+    let N = Math.ceil(Math.pow((Math.abs(M) * (upper - lower) ** 5) / (180 * error), 1 / 4)) + 30
+    let q = (upper - lower) / N
     let sum = 0
-    for (let n=0;n<=N-1;n++){
-      sum += integrand(lower+n*q)+2*integrand(lower+(n+0.5)*q)
+    for (let n = 0; n <= N - 1; n++) {
+      sum += integrand(lower + n * q) + 2 * integrand(lower + (n + 0.5) * q)
     }
-    return q/3*(0.5*(integrand(upper)-integrand(lower))+sum)
+    return q / 3 * (0.5 * (integrand(upper) - integrand(lower)) + sum)
   }
-  
+
 }
--- README.md
+++ README.md
@@ -7,50 +7,60 @@ Head over to the [NumCalcJS](https://github.com/Bobingstern/NumCalcJs/tree/main/
 ## Usage
 
 ### Maximum and Minimum
-`NumCalc.maximum(f,a,b,error)`
 
-Return the x value of the maximum of a function `f` over a given interval `a,b` with an optional `error` argument using the Golden Section Search. Default error `1e-4`
+`NumCalc.maximum(f: function, a: number, b: number, error: number = 1e-4): number`
 
-`NumCalc.minimum(f,a,b,error)`
+`NumCalc.minimum(f: function, a: number, b: number, error: number = 1e-4): number`
 
-Return the x value of the minimum of a function `f` over a given interval `a,b` with an optional `error` argument using the Golden Section Search. Default error `1e-4`
+Return the `x` value of the maximum/minimum of a function `f` over a given interval `a, b` with an optional `error` argument using the Golden Section Search. Default value of error is `1e-4`.
 
 ```js
-function f(x){
-  return Math.sin(x)
-}
-console.log(NumCalc.maximum(f,1.3,5.6),NumCalc.minimum(f,1.3,5.6)) //1.57079... 4.71239...
+console.log(
+  NumCalc.maximum(Math.sin, 1.3, 5.6),
+  NumCalc.minimum(Math.sin, 1.3, 5.6)
+); // 1.57079... 4.71239...
+```
+
+### NDerivative
+
+`NumCalc.NDerivative(f: function, x: number, n: number): number`
+
+Returns the instant slope at `x` of the `n`th derivative of `f`. High order derivatives will lose precision due to floating point round off. This is countered by using Machine Epsilon but is not 100% perfect.
+
+```js
+console.log(
+  NumCalc.NDerivative(Math.sin, Math.PI / 2, 1) // 1st derivative of sin
+); // 0
 ```
 
 ### FDerivative
-`NumCalc.FDerivative(f,n)`
 
-Returns the nth derivative of `f` as a callable function. High order derivatives will lose precision due to floating point roud off. This is countered by using Machine Epsilon but is not 100% perfect.
+`NumCalc.FDerivative(f: function, n: number): function`
+
+Returns the `n`th derivative of `f` as a callable function. High order derivatives will lose precision due to floating point round off. This is countered by using Machine Epsilon but is not 100% perfect. This is just a wrapper around `NumCalc.NDerivative` to return a function.
 
 ```js
-function f(x){
-  return Math.sin(x) * Math.sqrt(1+x**2)
-}
-let derivative = NumCalc.FDerivative(f,4) //4th derivative of f
-console.log(derivative(3.53)) //2.27...
+let derivative = NumCalc.FDerivative(Math.sin, 1) // 1st derivative of sin
+console.log(derivative(Math.PI)) // -0.99999...
 ```
 
 ### NIntegrate
-`NumCalc.NIntegrate(f,a,b,error)`
+`NumCalc.NIntegrate(f: number, a: number, b: number, error: number = 1e-4): number`
 
-Computes the definite integral of `f` over a given interval `a,b` using Simpson's Rule and the 4th derivative of `f` to choose the number of partitions used using `error`. It can handle improper integrals too using bounds of `Infinity` or `-Infinity` Default error `1e-4`. Likely to fail if the bounds contain an asymptote
+Computes the definite integral of `f` over a given interval `a, b` using Simpson's Rule and the 4th derivative of `f` to choose the number of partitions used using `error`. It can handle improper integrals too using bounds of `Infinity` or `-Infinity`. The default value of error is `1e-4`. Likely to fail if the bounds contain an asymptote.
 
 ```js
-function f(x){
-  return Math.exp(-(x**2)) // Gaussian bump
+function f(x) {
+  return Math.exp(-(x**2)); // Gaussian bump
 }
 
-console.log(NumCalc.NIntegrate(f,0,Infinity)) //0.88622...
-console.log(NumCalc.NIntegrate(f,-Infinity,Infinity)) //1.77245...
-console.log(NumCalc.NIntegrate(f,-Infinity,0)) //0.88622...
-console.log(NumCalc.NIntegrate(f,-2.2,0.2)) //1.081941...
+console.log(NumCalc.NIntegrate(f, 0, Infinity)); // 0.88622...
+console.log(NumCalc.NIntegrate(f, -Infinity, Infinity)); // 1.77245...
+console.log(NumCalc.NIntegrate(f, -Infinity, 0)); // 0.88622...
+console.log(NumCalc.NIntegrate(f, -2.2, 0.2)); // 1.081941...
 ```
 
 ## Notes
+
 This library is not meant to be perfect or super efficient so expect bugs. You can make an issue and I'll try to fix it in a later release.
 
